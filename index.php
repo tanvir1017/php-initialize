c619eaf3b1474eb4572f82b31119c7c452b82df4
Tanvir Hossain <87421998+tanvir1017@users.noreply.github.com>
2024-01-20 13:22:47 +0600

    feat: 🎸 Initializing PHP for the first time

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP STARTER</title>
+</head>
+<body>
+    <?php
+    echo"<span>I love pizza</span> <br>"; // for show any output
+    echo"it's really good";
+?>
+</br>
+<button>Order Pizza </button>
+</body>
+</html>
